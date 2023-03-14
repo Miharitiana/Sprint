f005e363b0854a2b453df49ce67462e56eaaebb3
Miharitiana <miharitianarak@yahoo.com>
2023-03-14 07:21:50 +0300

    sprint2

---
+++ src/java/etu1826/framework/Mapping.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package etu1826.framework;
+
+/**
+ *
+ * @author MSI
+ */
+public class Mapping {
+    
+    String className;
+    String method;
+    
+    
+}
